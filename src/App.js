ffa6edc6ea73bddc583117f5f22d35a3e3f1fa10
dev-jkc <birateral@gmail.com>
2022-02-08 17:51:30 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,84 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className='header'>
+      <div className='header'>
         <button className='header-hamburger'>
-          <img src='img\ic\24\hamburger.svg' />
+          <img src='img\ic\default\hamburger.svg' alt='hamburger' />
         </button>
         <div className='container-header-logo'>
-          <img className='header-logo' src='img\logo_main.svg' />
+          <img className='header-logo' src='img\logo_main.svg' alt='logo' />
         </div>
         <button className='header-btn'>login</button>
-      </header>
+      </div>
+
+      <div className='main-contents'>
+        <div className='container-card'>
+          <div className='card-top'>
+            <img className='card-top-img' src='img\thumbnail_market.png' alt='thumbnail' />
+            <div className='card-top-title'>
+              <p>경메모장</p>
+              <div className='card-top-button'>
+                <span>바로가기</span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.4393 1.93934C13.0251 1.35355 13.9749 1.35355 14.5607 1.93934L23.5607 10.9393C24.1464 11.5251 24.1464 12.4749 23.5607 13.0607L14.5607 22.0607C13.9749 22.6464 13.0251 22.6464 12.4393 22.0607C11.8536 21.4749 11.8536 20.5251 12.4393 19.9393L20.3787 12L12.4393 4.06066C11.8536 3.47487 11.8536 2.52513 12.4393 1.93934Z" fill="currentColor" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className='card-top'>
+            <img className='card-top-img' src='img\thumbnail_market.png' alt='thumbnail' />
+            <div className='card-top-title'>
+              <p>경메모장</p>
+              <div className='card-top-button'>
+                <span>바로가기</span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.4393 1.93934C13.0251 1.35355 13.9749 1.35355 14.5607 1.93934L23.5607 10.9393C24.1464 11.5251 24.1464 12.4749 23.5607 13.0607L14.5607 22.0607C13.9749 22.6464 13.0251 22.6464 12.4393 22.0607C11.8536 21.4749 11.8536 20.5251 12.4393 19.9393L20.3787 12L12.4393 4.06066C11.8536 3.47487 11.8536 2.52513 12.4393 1.93934Z" fill="currentColor" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className='card-top'>
+            <img className='card-top-img' src='img\thumbnail_market.png' alt='thumbnail' />
+            <div className='card-top-title'>
+              <p>경메모장</p>
+              <div className='card-top-button'>
+                <span>바로가기</span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.4393 1.93934C13.0251 1.35355 13.9749 1.35355 14.5607 1.93934L23.5607 10.9393C24.1464 11.5251 24.1464 12.4749 23.5607 13.0607L14.5607 22.0607C13.9749 22.6464 13.0251 22.6464 12.4393 22.0607C11.8536 21.4749 11.8536 20.5251 12.4393 19.9393L20.3787 12L12.4393 4.06066C11.8536 3.47487 11.8536 2.52513 12.4393 1.93934Z" fill="currentColor" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className='card-top'>
+            <img className='card-top-img' src='img\thumbnail_market.png' alt='thumbnail' />
+            <div className='card-top-title'>
+              <p>경메모장</p>
+              <div className='card-top-button'>
+                <span>바로가기</span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.4393 1.93934C13.0251 1.35355 13.9749 1.35355 14.5607 1.93934L23.5607 10.9393C24.1464 11.5251 24.1464 12.4749 23.5607 13.0607L14.5607 22.0607C13.9749 22.6464 13.0251 22.6464 12.4393 22.0607C11.8536 21.4749 11.8536 20.5251 12.4393 19.9393L20.3787 12L12.4393 4.06066C11.8536 3.47487 11.8536 2.52513 12.4393 1.93934Z" fill="currentColor" />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className='card-top'>
+            <img className='card-top-img' src='img\thumbnail_market.png' alt='thumbnail' />
+            <div className='card-top-title'>
+              <p>경메모장</p>
+              <div className='card-top-button'>
+                <span>바로가기</span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.4393 1.93934C13.0251 1.35355 13.9749 1.35355 14.5607 1.93934L23.5607 10.9393C24.1464 11.5251 24.1464 12.4749 23.5607 13.0607L14.5607 22.0607C13.9749 22.6464 13.0251 22.6464 12.4393 22.0607C11.8536 21.4749 11.8536 20.5251 12.4393 19.9393L20.3787 12L12.4393 4.06066C11.8536 3.47487 11.8536 2.52513 12.4393 1.93934Z" fill="currentColor" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='footer'>
+        <p>© 2022. May2nd All rights reserved. hidden-street is not associated with NEXON Korea.</p>
+      </div>
     </div>
   );
 }
